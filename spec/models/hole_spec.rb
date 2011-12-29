9a1e7dfe7cdf2e56d005f10dbf6813c6f7b6d5fb
Ryan Ahearn <ryan@coshx.com>
2011-12-29 10:57:46 -0500

    Adds uniqueness and numericality validations to Hole #hole & #handicap

--- spec/models/hole_spec.rb
+++ spec/models/hole_spec.rb
@@ -11,6 +11,10 @@ describe Hole do
     it { should validate_presence_of :hole }
     it { should validate_presence_of :par }
     it { should validate_presence_of :score }
+    it { should validate_uniqueness_of :hole }
+    it { should validate_uniqueness_of :handicap }
+    it { should validate_numericality_of(:hole).less_than_or_equal_to 18 }
+    it { should validate_numericality_of(:handicap).less_than_or_equal_to 18 }
     context "when holed is a teebox" do
       subject { build :teebox_hole }
       it { subject.should be_valid }
