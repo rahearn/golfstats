174390d769cc08bad3dd4d451cf7229a003c9dad
Ryan Ahearn <ryan@coshx.com>
2012-01-09 14:27:03 -0500

    Adds course search to the homepage

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,7 @@
 class CoursesController < ApplicationController
 
   load_and_authorize_resource
+  before_filter :run_search, :only => :index
 
   def index
   end
@@ -16,6 +17,15 @@ class CoursesController < ApplicationController
       redirect_to @course
     else
       render :new
+    end
+  end
+
+  private
+
+  def run_search
+    if params[:q].present?
+      @courses = @courses.search params[:q]
+      render :search_results
     end
   end
 
