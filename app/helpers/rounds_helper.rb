e9251dd9c802b448d5ecd1c3a192b25974ed2a7d
Ryan Ahearn <ryan@coshx.com>
2012-01-09 18:31:26 -0500

    Loads round/scorecard defaults from teebox if given

--- app/helpers/rounds_helper.rb
+++ app/helpers/rounds_helper.rb
@@ -4,10 +4,24 @@ module RoundsHelper
     if round.scorecard.present?
       round.scorecard
     else
-      Scorecard.new(:round => round).tap do |sc|
-        (1..18).each do |hole|
-          sc.holes.build :hole => hole
-        end
+      Scorecard.new(:round => round, :tees => tees_value).tap do |sc|
+        build_holes sc
+      end
+    end
+  end
+
+  def tees_value
+    (@teebox.try(:tees) || '').humanize
+  end
+
+  def build_holes(scorecard)
+    if @teebox.present?
+      @teebox.holes.each do |hole|
+        scorecard.holes.build hole.attributes
+      end
+    else
+      (1..18).each do |hole|
+        scorecard.holes.build :hole => hole
       end
     end
   end
