3b47f57d61209c5ded76a9dc7c75dbf62b0d897f
Ryan Ahearn <ryan@coshx.com>
2012-06-07 10:29:00 -0400

    Adds error messages on open_id user creation

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,19 +1,19 @@
 module Users
   class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-    prepend_before_filter :require_no_authentication, :except => :developer
-    before_filter         :ensure_development,        :only   => :developer
+    prepend_before_filter :require_no_authentication, except: :developer
+    before_filter         :ensure_development,        only:   :developer
     before_filter         :load_auth_and_user
     skip_authorization_check
 
     def open_id
       @user.email = @auth.info.email if @auth.info.email.present?
       @user.name  = @auth.info.name  if @auth.info.name.present?
-      if @user.save
+      if @user.save || @user.persisted?
         sign_in_and_redirect @user
       else
-        Rails.logger.error "Could not log in #{@user.new_record? ? 'new' : 'existing'} user: #{@user.errors.full_messages}"
-        redirect_to new_user_session_path
+        Rails.logger.error "Could not create new user: #{@user.errors.full_messages}"
+        redirect_to new_user_session_path, alert: "There was an error registering you. #{@user.errors.full_messages.join('. ')}."
       end
     end
     alias :google    :open_id
