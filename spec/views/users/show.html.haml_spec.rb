2d5949faa45ac7d224f8a2937947f0a0617452c2
Ryan Ahearn <ryan@coshx.com>
2012-07-06 15:42:49 -0400

    Adds edit profile views

--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe "users/show" do
   let(:user) { build_stubbed :full_user }
   before(:each) do
+    view.stub(:can?).with(:edit, user).and_return true
     assign :user, user
     render
   end
@@ -13,5 +14,9 @@ describe "users/show" do
 
   it "displays the user's openid" do
     rendered.should include "<em>OpenId:</em>\n#{user.openid_uid}"
+  end
+
+  it "displays a link to the edit profile page" do
+    rendered.should include %{<a href="#{edit_user_path user}">Edit Profile</a>}
   end
 end
