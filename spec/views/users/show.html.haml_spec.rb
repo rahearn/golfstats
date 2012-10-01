a53246f34c9a9207cf1ba4f903af5a04e3a1333c
Ryan Ahearn <ryan@coshx.com>
2012-10-01 16:20:44 -0400

    Displays the openid provider if one of the 3 pre-baked ones

--- spec/views/users/show.html.haml_spec.rb
+++ spec/views/users/show.html.haml_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe "users/show" do
-  let(:user) { build_stubbed :full_user }
+  let(:user) { build_stubbed :full_user, openid_provider: 'open_id' }
   before(:each) do
     view.stub(:can?).with(:edit, user).and_return true
     assign :user, user
@@ -9,11 +9,19 @@ describe "users/show" do
   end
 
   it "displays the user's email" do
-    rendered.should include "<em>Email:</em>\n#{user.email}"
+    rendered.should =~ /<em>Email:<\/em>\s+#{user.email}/
   end
 
   it "displays the user's openid" do
-    rendered.should include "<em>OpenId:</em>\n#{user.openid_uid}"
+    rendered.should =~ /<em>OpenId:<\/em>\s+#{user.openid_uid}/
+  end
+
+  context "facebook user" do
+    let(:user) { build_stubbed :full_user, openid_provider: 'facebook' }
+
+    it "displays the openid provider" do
+      rendered.should =~ /<em>OpenId:<\/em>\s+Facebook:/
+    end
   end
 
   it "displays a link to the edit profile page" do
