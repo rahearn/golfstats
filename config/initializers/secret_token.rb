cb80476b8d249c8fa1d4b6bc05d870c24977228a
Ryan Ahearn <ryan@coshx.com>
2013-01-04 11:09:50 -0500

    Allows setting secret token via ENV var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Golfstats::Application.config.secret_token = 'b976692597d919eb9bf64a5ef78dddfd639d6310146ee69919ec3b3e9275287845c05188f90fdd855c3112bdc239449a0e76e84b2523379dcd9de75a628a0e15'
+Golfstats::Application.config.secret_token = ENV['SECRET_TOKEN'] || 'b976692597d919eb9bf64a5ef78dddfd639d6310146ee69919ec3b3e9275287845c05188f90fdd855c3112bdc239449a0e76e84b2523379dcd9de75a628a0e15'
